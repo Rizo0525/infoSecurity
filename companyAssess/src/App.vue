60f8caafc2325f30ce9307ecdbaa12840c76e36d
rizo <h2434530141@icloud.com>
2022-12-01 15:36:17 +0800

    1

---
+++ companyAssess/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <nav_menu></nav_menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Menu from "./components/Menu.vue";
+export default {
+  name: 'app',
+  components: {
+      nav_menu:Menu
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+.el-aside{
+  background: skyblue;
+  height: 100vh;
+}
+.el-header{
+  background: blueviolet;
+  padding: 0;
+}
+.el-main{
+  background: red;
+  padding: 0;
+}
+</style>
---
+++ companyAssess/src/components/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row>
+    <el-col>
+      <h2>评估系统</h2>
+      <el-menu
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          default-active="1-4-1"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <span>NaviGator One</span>
+          </template>
+
+          <el-menu-item index="1-1">item-One</el-menu-item>
+          <el-menu-item index="1-2">item-Two</el-menu-item>
+          <el-menu-item index="1-3">item-Three</el-menu-item>
+          <el-sub-menu index="1-4">
+            <template #title><span>item-Four</span></template>
+            <el-menu-item index="1-4-1">item-Four-one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <span>NaviGator Two</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+export default {
+  name: "Menu"
+}
+const handleOpen = (key,keyPath)=>{
+  console.log(key, keyPath);
+}
+const handleClose = (key,keyPath)=>{
+  console.log(key, keyPath);
+}
+</script>
+
+<style scoped>
+
+</style>
