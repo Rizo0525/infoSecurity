a823dea1fa25097aafd069589457745de6908159
rizo <h2434530141@icloud.com>
2022-12-27 15:37:42 +0800

    12

--- companyAssess/src/components/result/finalresult.vue
+++ companyAssess/src/components/result/finalresult.vue
@@ -2,35 +2,35 @@
   <div>
     <h2>资产风险值表</h2>
     <el-table :data="tableData" style="width: 100%" border height="200">
-      <el-table-column prop="id" label="序号"/>
+<!--      <el-table-column prop="id" label="序号"/>-->
       <el-table-column  label="资产名称">
         <template #default="scope">
-          <span>{{scope.row.assets.name}}</span>
+          <span>{{scope.row.vul.assets.name}}</span>
         </template>
       </el-table-column>
       <el-table-column  label="重要性赋值">
         <template #default="scope">
-          <span>{{scope.row.assets.importance}}</span>
+          <span>{{scope.row.vul.assets.importance}}</span>
         </template>
       </el-table-column>
       <el-table-column  label="威胁">
         <template #default="scope">
-          <span>{{scope.row.threaten.type}}</span>
+          <span>{{scope.row.vul.threaten.type}}</span>
         </template>
       </el-table-column>
       <el-table-column label="威胁赋值">
         <template #default="scope">
-          <span>{{scope.row.threaten.value}}</span>
+          <span>{{scope.row.vul.threaten.value}}</span>
         </template>
       </el-table-column>
       <el-table-column  label="脆弱性">
         <template #default="scope">
-          <span>{{scope.row.type}}</span>
+          <span>{{scope.row.vul.type}}</span>
         </template>
       </el-table-column>
       <el-table-column label="脆弱性赋值">
         <template #default="scope">
-          <span>{{scope.row.value}}</span>
+          <span>{{scope.row.vul.value}}</span>
         </template>
       </el-table-column>
       <el-table-column  label="可能性">
@@ -40,7 +40,7 @@
       </el-table-column>
       <el-table-column  label="损失">
         <template #default="scope">
-          <span>{{scope.row.loss}}</span>
+          <span>{{scope.row.los}}</span>
         </template>
       </el-table-column>
       <el-table-column  label="风险值">
@@ -52,7 +52,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import {ref, reactive} from "vue";
 import {projectStore} from "../../stores/project";
 const proStore = projectStore()
@@ -67,25 +67,17 @@ fetch(`/api/calculate/getResult?proid=${proStore.proId}`,{
   // res.message.forEach((val,index)=>{
   //   console.log(val, index);
   // })
-  fetch(`/api/vulnerable/queryAll?proid=${proStore.proId}`,{
-    method:'get',
-    credentials: 'include',
-  }).then((res1)=>{
-    return res1.json()
-  }).then((res1)=>{
-
-    tableData.value = res.message
-
-    for(let i=0;i<tableData.value.length;i++){
-      // console.log(tableData.value[i]);
-      tableData.value[i].threaten = res1.message[i].threaten
-      // res.message[i].threaten = res1.message[i].threaten
-      // console.log(res1.message[i].type);
-      res.message[i].type = res1.message[i].type
-      res.message[i].value = res1.message[i].value
-    }
-    console.log(tableData.value);
-  })
+  tableData.value = res.message
+  console.log(tableData.value);
+  // fetch(`/api/vulnerable/queryAll?proid=${proStore.proId}`,{
+  //   method:'get',
+  //   credentials: 'include',
+  // }).then((res1)=>{
+  //   return res1.json()
+  // }).then((res1)=>{
+  //
+  //
+  // })
 })
 
 
